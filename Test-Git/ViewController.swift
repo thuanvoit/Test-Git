390f05745b1c2eb468d174fbdbefe832d2a9414b
Thuan Vo <thuanvo09@hotmail.com>
2020-12-31 21:58:29 -0500

    Initial Commit

---
+++ Test-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-Git
+//
+//  Created by Thuan Vo on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
